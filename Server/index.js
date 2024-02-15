be4e754be5e8e14fae7992e0a488d27a3520e5ea
kiokoeric <kiokoerick040@gmail.com>
2024-02-15 14:13:44 +0300

    Server Side Changes

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://story-keeper-sz52.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
